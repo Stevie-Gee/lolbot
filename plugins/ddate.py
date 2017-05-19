e7be009b258c1d05a0fd1beab7fee54c5ee2637c
Tayck <tayck@tayck.com>
2017-05-20 00:28:26 +1200

    Refactor ddate plugin.

--- plugins/ddate.py
+++ plugins/ddate.py
@@ -23,8 +23,8 @@ def call(msg):
     if istib:
         result = "Today is St. Tib's day in the YOLD %s" % year
     else:
-        result = ("Today is %s, the %s day of %s in the YOLD %s" % 
-                  (WEEKDAYS[weekday], day, SEASONS[season], year))
+        result = ("Today is %s, the %s%s day of %s in the YOLD %s" %
+                  (WEEKDAYS[weekday], day, ordinalise(day), SEASONS[season], year))
     
     channel_id = msg.get("d").get("channel_id")
     config.HTTP_SESSION.post(
@@ -34,7 +34,7 @@ def call(msg):
 
 def ddate():
     """Return today's discordian date as a tuple of
-    (ordinal day of month, day of week, season, year, St Tibbs Day <bool>)
+    (day of month, day of week, season, year, St Tibbs Day <bool>)
     """
     date = time.localtime()
     istib = False
@@ -54,18 +54,22 @@ def ddate():
     year += 1166
     day = day % 73 + 1
     
-    if day in (11, 12, 13):
-        day = "%sth" % day
-    elif day % 10 == 1:
-        day = "%sst" % day
-    elif day % 10 == 2:
-        day = "%snd" % day
-    elif day % 10 == 3:
-        day = "%srd" % day
-    else:
-        day = "%sth" % day
-    
     return day, weekday, season, year, istib
+
+def ordinalise(number):
+    """Given a cardinal number less than 100, respond with the ordinal suffix.
+    e.g. 1 -> st, 12 -> th, 53 -> rd.
+    """
+    if number in (11, 12, 13):
+        return "th"
+    elif number % 10 == 1:
+        return "st"
+    elif number % 10 == 2:
+        return "nd"
+    elif number % 10 == 3:
+        return "rd"
+    else:
+        return "th"
 
 COMMANDS = {
     "ddate": call,
