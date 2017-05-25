34fb76484569d333eb7ef80054743cff42e805ba
Tayck <tayck@tayck.com>
2017-05-25 21:29:16 +1200

    Add command to get a user's discord ID.

---
+++ plugins/getid.py
@@ -0,0 +1,28 @@
+"""
+Get a user's discord ID without having to trawl through JSON.
+"""
+
+import re
+import bot_utils
+
+def command(msg):
+    """If the command is !uid <@mention>, return ID of the mentioned user.
+    Otherwise, return the uid of the user who sent the message.
+    """
+    content = msg.get("d").get("content")
+    if ' ' in content:
+        arg = content.split()[1]
+        matches = re.match(r'\<\@(\d+)\>', arg)
+        if matches:
+            reply = "Discord UserID %s" % matches.group(1)
+        else:
+            reply = "Invalid format"
+    else:
+        userid = msg["d"]["author"]["id"]
+        reply = "Discord UserID %s" % userid
+    bot_utils.reply(msg, reply)
+
+COMMANDS = {
+    "uid": command,
+    "userid": command,
+}
